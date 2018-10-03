e80dbe1dd7f37df8870ebf1ff962331419eadf46
funfun313 <43829418+funfun313@users.noreply.github.com>
2018-10-03 16:28:34 -0500

    pirate name

---
+++ pirate.py
@@ -0,0 +1,10 @@
+class Pirate:
+    first = ""
+    second = ""
+    third = ""
+    def __init__(self,first, second, third):
+        x=0
+        #put parameters into properties
+    def name(self):
+        x=0
+        #this function will figure out and return the name
