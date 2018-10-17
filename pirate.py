28c617eebf166b5f7094d60dc94b4e13f147fd2b
funfun313 <43829418+funfun313@users.noreply.github.com>
2018-10-17 16:31:04 -0500

    pirate name v3

--- pirate.py
+++ pirate.py
@@ -1,4 +1,5 @@
 import random
+from tkinter import *
 class Pirate:
     first = ""
     second = ""
@@ -20,4 +21,33 @@ class Pirate:
         z = random.randint(0,len(self.thirdList)-1)
         pirateName = self.firstList[x] + " " + self.secondList[y] + " " + self.thirdList[z]
         return pirateName
-        
+myfont="Arial 14 bold"
+root = Tk()
+#register image
+banner= PhotoImage(file="pirate-banner.gif")
+banner = banner.subsample(2,2)
+
+#creating controls
+title = Label(root, text="Discover Your Pirate Name", font="Arial 20 bold")
+fLabel =Label(root, text="First name:", font=myfont)
+sLabel =Label(root, text="Second name:", font=myfont)
+tLabel =Label(root, text="Third name:", font=myfont)
+fBox= Entry(root, font=myfont)
+sBox= Entry(root, font=myfont)
+tBox= Entry(root, font=myfont)
+btn= Button(root, text ="Show Me My Name!", font="Arial 16 bold", bg="#eeddff")
+output = Label(root, font="Gabriola 26", image=banner)
+
+
+#adding them to screen
+title.grid(row=0,column=0,columnspan=2)
+fLabel.grid(row=1,column=0)
+sLabel.grid(row=2,column=0)
+tLabel.grid(row=3,column=0)
+fBox.grid(row=1, column=1)
+sBox.grid(row=2, column=1)
+tBox.grid(row=3, column=1)
+btn.grid(row=4,column=0, columnspan=2)
+output.grid(row=5,column=0, columnspan=2)
+
+root.mainloop()
